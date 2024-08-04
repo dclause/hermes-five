ee29ae98e68bcfac642a64b754cee512753e2177
Dominique CLAUSE <dom.clause@gmail.com>
2024-08-04 23:16:48 +0200

    Use proc-macro-name to determine crate name.

--- hermes-macros/src/internals/helpers.rs
+++ hermes-macros/src/internals/helpers.rs
@@ -1,19 +1,50 @@
+use proc_macro2::Span;
 use proc_macro2::TokenStream;
+use proc_macro_crate::FoundCrate;
 use quote::quote;
+use syn::Ident;
 
 /// Determines what crate name should be used to refer to `hermes_core`.
 /// crate::... or hermes_five::... depending.
 pub fn hermes_five_crate_path() -> TokenStream {
-    let is_internal = std::env::var("CARGO_CRATE_NAME")
-        .map(|pkg_name| pkg_name == "hermes_five")
-        .unwrap_or_default();
+    // let is_internal = std::env::var("CARGO_CRATE_NAME")
+    //     .map(|pkg_name| pkg_name == "hermes_five")
+    //     .unwrap_or_default();
+    //
+    // #[cfg(doctest)]
+    // let is_internal = false;
+    //
+    // match is_internal {
+    //     true => quote!(crate),
+    //     false => quote!(hermes_five),
+    // }
 
-    #[cfg(doctest)]
-    let is_internal = false;
+    // let found_crate = crate_name("hermes-five").expect("hermes-five is present in `Cargo.toml`");
+    //
+    // match found_crate {
+    //     FoundCrate::Itself => quote!(crate),
+    //     FoundCrate::Name(name) => {
+    //         let ident = Ident::new(&name, Span::call_site());
+    //         quote!(#ident)
+    //     }
+    // }
 
-    match is_internal {
-        true => quote!(crate),
-        false => quote!(hermes_five),
+    println!(
+        "######### crate name: {:?}  / {:?}",
+        proc_macro_crate::crate_name("hermes-five"),
+        std::env::var("CARGO_CRATE_NAME").as_deref()
+    );
+
+    match (
+        proc_macro_crate::crate_name("hermes-five"),
+        std::env::var("CARGO_CRATE_NAME").as_deref(),
+    ) {
+        (Ok(FoundCrate::Itself), Ok("hermes_five")) => quote!(crate),
+        (Ok(FoundCrate::Name(name)), _) => {
+            let ident = Ident::new(&name, Span::call_site());
+            quote!(#ident)
+        }
+        _ => quote!(hermes_five),
     }
 }
 
@@ -26,13 +57,13 @@ mod tests {
     #[test]
     fn test_hermes_five_crate_path_internal() {
         // Set the environment variable
-        env::set_var("CARGO_CRATE_NAME", "hermes_five");
+        env::set_var("CARGO_CRATE_NAME", "hermes-five");
 
         // Call the function
         let result = hermes_five_crate_path().to_string();
 
         // Assert the result
-        assert_eq!(result, "crate");
+        assert_eq!(result, "hermes_five");
 
         // Clean up the environment variable
         env::remove_var("CARGO_CRATE_NAME");
