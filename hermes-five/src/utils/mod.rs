3a93a7e21bda5aa211c3d835706ab810c57a5b96
Dominique CLAUSE <dom.clause@gmail.com>
2024-09-16 18:22:19 +0200

    Add serde is_default() check for serialization.

--- hermes-five/src/utils/mod.rs
+++ hermes-five/src/utils/mod.rs
@@ -1,3 +1,4 @@
+pub use log;
 #[cfg(test)]
 pub use serial_test;
 pub use tokio;
@@ -13,4 +14,31 @@ mod state;
 pub mod events;
 pub mod scale;
 pub mod task;
-// pub mod task2;
+
+#[cfg(feature = "serde")]
+// Helper for serialize skip method.
+pub(crate) fn is_default<T: Default + PartialEq>(t: &T) -> bool {
+    t == &T::default()
+}
+
+#[cfg(feature = "serde")]
+#[cfg(test)]
+mod tests {
+    use crate::utils::is_default;
+    use crate::utils::State::Boolean;
+
+    #[test]
+    fn test_is_default() {
+        // Bool
+        assert_eq!(is_default(&true), false);
+        assert_eq!(is_default(&false), true);
+        // String
+        assert_eq!(is_default(&String::new()), true);
+        assert_eq!(is_default(&String::from("test")), false);
+        // usize
+        assert_eq!(is_default(&0), true);
+        assert_eq!(is_default(&69), false);
+
+        // ....
+    }
+}
